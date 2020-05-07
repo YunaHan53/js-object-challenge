2db58e8f30efcb59d5f034579d7635e02665cf72
YunaH <YunaH503@gmail.com>
2020-05-06 23:43:26 -0400

    Changed all the concat to template literals (ES6)

--- js/report.js
+++ js/report.js
@@ -8,12 +8,12 @@ function print(message) {
 
 for (let i = 0; i < students.length; i++) {
   student = students[i]
-  html += '<h2>Name: ' + student.name + '</h2>'
-  html += '<ul>'
-  html += '<li>Track: ' + student.track + '</li>'
-  html += '<li>Achievements: ' + student.achievements + '</li>'
-  html += '<li>Points: ' + student.points + '</li>'
-  html += '</ul>'
+  html += `<h2>Name: ${student.name}</h2>`
+  html += `<ul>`
+  html += `<li>Track: ${student.track}</li>`
+  html += `<li>Achievements: ${student.achievements}</li>`
+  html += `<li>Points: ${student.points}</li>`
+  html += `</ul>`
 }
 
 print(html)
