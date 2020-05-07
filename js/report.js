f3060e6d21db20c818f3b4706266a701013afe3e
YunaH <YunaH503@gmail.com>
2020-05-06 23:37:48 -0400

    Added report.js file to print out the students report

---
+++ js/report.js
@@ -0,0 +1,19 @@
+let html = ''
+let student
+
+function print(message) {
+  let outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+for (let i = 0; i < students.length; i++) {
+  student = students[i]
+  html += '<h2>Name: ' + student.name + '</h2>'
+  html += '<ul>'
+  html += '<li>Track: ' + student.track + '</li>'
+  html += '<li>Achievements: ' + student.achievements + '</li>'
+  html += '<li>Points: ' + student.points + '</li>'
+  html += '</ul>'
+}
+
+print(html)
