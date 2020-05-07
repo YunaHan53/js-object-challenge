234513c07c3d20c5973f83e315d9bc6758d6cfb9
YunaH <YunaH503@gmail.com>
2020-05-06 22:59:23 -0400

    Created students array with objects

---
+++ js/students.js
@@ -0,0 +1,33 @@
+const students = [
+  {
+    name: 'Ashley',
+    track: 'Full Stack Development',
+    achievements: 102,
+    points: 9500
+  },
+  {
+    name: 'Choi',
+    track: 'Data Science',
+    achievements: 64,
+    points: 5000
+  },
+  {
+    name: 'Emmy',
+    track: 'Front End Web Development',
+    achievements: 78,
+    points: 6200
+  },
+  {
+    name: 'Craig',
+    track: 'React Development',
+    achievements: 147,
+    points: 15000
+  },
+  {
+    name: 'InQ',
+    track: 'Python Development',
+    achievements: 99,
+    points: 9000
+  },
+
+]
